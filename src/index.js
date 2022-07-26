885e07f6ef8ce9fd0a1fcc3f33fb69f83a3b591b
zhangchuqiang <1033314450@qq.com>
2022-07-26 10:30:49 +0800

    fix:修改没有通过app.use调用时直接调用preview报错的问题

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import { createApp } from 'vue'
 import PreviewContent from './index.vue'
 
-let state = null
-
+let state = null // 预览组件内部的状态数据
+/**
+ * 通过use调用时挂载到vue实例
+ * @param {Object} App
+ */
 export default function install(App) {
-  const instance = createApp(PreviewContent)
-  const box = document.createElement('div')
-  document.body.appendChild(box)
-  state = instance.mount(box)
-
+  initialize()
   App.config.globalProperties.$preview = preview
 }
 /**
@@ -16,12 +15,15 @@ export default function install(App) {
  * @param {Array} list 需要预览的图片数组 非必传 如果不传的话 current必须为url 不能为索引
  * @param {String} key 如果list里面的item是图片的url 则不需要，如果是对象的话，需传图片的在对象中的key。
  */
- export function preview(current = '', list = [], key = '') {
+export function preview(current = '', list = [], key = '') {
+  if (state === null) {
+    initialize()
+  }
   if (current === '' || current === null || current === undefined) {
     throw 'Vue3PreviewImage：请传入参数'
   } else if (typeof current === 'number') {
-    if (!list.length) {
-      throw 'Vue3PreviewImage：参数错误，如果请在第二个参数中传入数组'
+    if (!list || !list.length) {
+      throw 'Vue3PreviewImage：参数错误，第一个参数为索引时，请在第二个参数中传入数组'
     }
     state.currentImg = key ? list[current][key] : list[current]
   } else {
@@ -33,4 +35,13 @@ export default function install(App) {
   state.imgList = list
   state.imgKey = key
   state.show = true
+}
+/**
+ * 初始化
+ */
+function initialize() {
+  const instance = createApp(PreviewContent)
+  const box = document.createElement('div')
+  document.body.appendChild(box)
+  state = instance.mount(box)
 }
